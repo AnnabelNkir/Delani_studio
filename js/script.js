2785798edec7734cd295f37cc7e65c9f77007469
AnnabelNkir <annabel.micheni@student.moringaschool.com>
2021-09-14 00:11:58 +0300

    Add form validation and functionality to it

--- js/script.js
+++ js/script.js
@@ -54,4 +54,22 @@ $('.work8').mouseover(function () {
 }).mouseout(function () {
     $('.work8overlay').hide();
 });
+
+$("form#form1").on('submit',function(event){
+  event.preventDefault();
+  var name = $("input#name").val();
+  var email = $("input#email").val();
+  var message = $("textarea#message").val();
+
+  if ($("input#name").val() && $("input#email").val() && $("input#message").val() ){
+      alert ("Hello " + name + ", Thank you for reaching out, we'll get back to you shortly...");
+  }
+  else if($("input#name").val() && $("input#email").val()){
+      alert("Thank you for reaching us. If you wish to leave a message please type in on the message section..");
+  }
+ else{
+   alert("Please provide correct name or email")
+ }
+});
+
 });
